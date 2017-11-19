6c13449810139ba4bd05b6d843d998a8624b0b92
Masatake YAMATO <yamato@redhat.com>
2017-11-19 13:28:10 +0900

    kvm: decode arguments for KVM_GET_DIRTY_LOG
    * kvm.c (kvm_ioctl_get_dirty_log): New function.
    (kvm_ioctl): Call `kvm_ioctl_get_dirty_log'.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>

--- kvm.c
+++ kvm.c
@@ -81,6 +81,20 @@ kvm_ioctl_irq_line_status(struct tcb *const tcp, const kernel_ulong_t arg)
 	return RVAL_IOCTL_DECODED;
 }
 
+static int
+kvm_ioctl_get_dirty_log(struct tcb *const tcp, const kernel_ulong_t arg)
+{
+	struct kvm_dirty_log dirty_log;
+
+	if (umoven(tcp, arg, sizeof(dirty_log), &dirty_log) < 0)
+		return RVAL_DECODED;
+
+	tprintf(", {slot=%d, dirty_bitmap=%p}",
+		dirty_log.slot, dirty_log.dirty_bitmap);
+
+	return RVAL_IOCTL_DECODED;
+}
+
 int
 kvm_ioctl(struct tcb *const tcp, const unsigned int code, const kernel_ulong_t arg)
 {
@@ -91,6 +105,8 @@ kvm_ioctl(struct tcb *const tcp, const unsigned int code, const kernel_ulong_t a
 		return RVAL_DECODED;
 	case KVM_IRQ_LINE_STATUS:
 		return kvm_ioctl_irq_line_status(tcp, arg);
+	case KVM_GET_DIRTY_LOG:
+		return kvm_ioctl_get_dirty_log(tcp, arg);
 	default:
 		return RVAL_DECODED;
 	}
