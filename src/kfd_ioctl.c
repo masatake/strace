edb45cf2654e95f8022056141ac0e81345c94a45
Zenithal <i@zenithal.me>
2023-02-17 21:22:01 +0800

    Add kfd ioctls that do not have corresponding test

--- src/kfd_ioctl.c
+++ src/kfd_ioctl.c
@@ -170,6 +170,53 @@ print_amdkfd_ioc_get_clock_counters(struct tcb *const tcp,
 	return RVAL_IOCTL_DECODED;
 }
 
+static int
+print_amdkfd_ioc_get_process_apertures(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_get_process_apertures_args args;
+
+	if (entering(tcp))
+		return 0;
+
+	tprint_arg_next();
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	tprint_struct_begin();
+	PRINT_FIELD_PTR(args, process_apertures); /* TODO: print array */
+	tprint_struct_next();
+	PRINT_FIELD_U(args, num_of_nodes);
+	tprint_struct_end();
+
+	return RVAL_IOCTL_DECODED;
+}
+
+static int
+print_amdkfd_ioc_update_queue(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_update_queue_args args;
+
+	tprint_arg_next();
+	if (!umove_or_printaddr(tcp, arg, &args)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, ring_base_address);
+		tprint_struct_next();
+
+		PRINT_FIELD_X(args, queue_id);
+		tprint_struct_next();
+		PRINT_FIELD_X(args, ring_size);
+		tprint_struct_next();
+		PRINT_FIELD_U(args, queue_percentage);
+		tprint_struct_next();
+		PRINT_FIELD_U(args, queue_priority);
+		tprint_struct_end();
+	}
+
+	return RVAL_IOCTL_DECODED;
+}
+
 #include "xlat/kfd_event_type.h"
 
 static int
@@ -242,6 +289,60 @@ print_amdkfd_ioc_set_event(struct tcb *const tcp,
 	}
 
 	return RVAL_IOCTL_DECODED;
+}
+
+static int
+print_amdkfd_ioc_reset_event(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_reset_event_args args;
+
+	tprint_arg_next();
+	if (!umove_or_printaddr(tcp, arg, &args)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, event_id);
+		tprint_struct_end();
+	}
+
+	return RVAL_IOCTL_DECODED;
+}
+
+#include "xlat/kfd_wait_result.h"
+
+static int
+print_amdkfd_ioc_wait_events(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_wait_events_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		tprint_struct_begin();
+		PRINT_FIELD_XVAL(args, wait_result, kfd_wait_result, "KFD_IOC_WAIT_RESULT_???");
+		tprint_struct_end();
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_PTR(args, events_ptr);
+	tprint_struct_next();
+	PRINT_FIELD_X(args, num_events);
+	tprint_struct_next();
+	PRINT_FIELD_X(args, wait_for_all);
+	tprint_struct_next();
+	PRINT_FIELD_U(args, timeout);
+	tprint_struct_end();
+	return 0;
 }
 
 static int
@@ -499,6 +600,279 @@ print_amdkfd_ioc_unmap_memory_from_gpu(struct tcb *const tcp,
 }
 
 static int
+print_amdkfd_ioc_set_cu_mask(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_set_cu_mask_args args;
+
+	tprint_arg_next();
+	if (!umove_or_printaddr(tcp, arg, &args)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, queue_id);
+		tprint_struct_next();
+		PRINT_FIELD_U(args, num_cu_mask);
+		tprint_struct_next();
+		PRINT_FIELD_PTR(args, cu_mask_ptr);
+		tprint_struct_end();
+	}
+
+	return RVAL_IOCTL_DECODED;
+}
+
+static int
+print_amdkfd_ioc_get_queue_wave_state(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_get_queue_wave_state_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, ctl_stack_used_size);
+		tprint_struct_next();
+		PRINT_FIELD_X(args, save_area_used_size);
+		tprint_struct_end();
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_X(args, ctl_stack_address);
+	tprint_struct_next();
+	PRINT_FIELD_U(args, queue_id);
+	tprint_struct_end();
+	return 0;
+}
+
+static int
+print_amdkfd_ioc_get_dmabuf_info(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_get_dmabuf_info_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, size);
+		tprint_struct_next();
+		PRINT_FIELD_X(args, metadata_size);
+		tprint_struct_next();
+		PRINT_FIELD_X(args, gpu_id);
+		tprint_struct_next();
+		PRINT_FIELD_FLAGS(args, flags, kfd_alloc_mem_flags, "KFD_IOC_ALLOC_MEM_FLAGS_???");
+		tprint_struct_end();
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_PTR(args, metadata_ptr);
+	tprint_struct_next();
+	PRINT_FIELD_X(args, metadata_size);
+	tprint_struct_next();
+	PRINT_FIELD_FD(args, dmabuf_fd, tcp);
+	tprint_struct_end();
+	return 0;
+}
+
+static int
+print_amdkfd_ioc_import_dmabuf(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_import_dmabuf_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, handle);
+		tprint_struct_end();
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_X(args, va_addr);
+	tprint_struct_next();
+	PRINT_FIELD_X(args, gpu_id);
+	tprint_struct_next();
+	PRINT_FIELD_FD(args, dmabuf_fd, tcp);
+	tprint_struct_end();
+	return 0;
+}
+
+static int
+print_amdkfd_ioc_alloc_queue_gws(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_alloc_queue_gws_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, first_gws);
+		tprint_struct_end();
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_X(args, queue_id);
+	tprint_struct_next();
+	PRINT_FIELD_X(args, num_gws);
+	tprint_struct_end();
+	return 0;
+}
+
+static int
+print_amdkfd_ioc_smi_events(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_smi_events_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		tprint_struct_begin();
+		PRINT_FIELD_FD(args, anon_fd, tcp);
+		tprint_struct_end();
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_X(args, gpuid);
+	tprint_struct_end();
+	return 0;
+}
+
+#include "xlat/kfd_svm_attr_type.h"
+#include "xlat/kfd_svm_location.h"
+#include "xlat/kfd_svm_flag.h"
+
+static void
+print_kfd_svm_attribute(const struct kfd_ioctl_svm_attribute *attr)
+{
+	tprint_struct_begin();
+	PRINT_FIELD_XVAL(*attr, type, kfd_svm_attr_type, "KFD_IOCTL_SVM_ATTR_???");
+	tprint_struct_next();
+	switch (attr->type) {
+	case KFD_IOCTL_SVM_ATTR_PREFERRED_LOC:
+	case KFD_IOCTL_SVM_ATTR_PREFETCH_LOC:
+		PRINT_FIELD_XVAL(*attr, value, kfd_svm_location, "KFD_IOCTL_SVM_LOCATION_???");
+		break;
+	case KFD_IOCTL_SVM_ATTR_SET_FLAGS:
+	case KFD_IOCTL_SVM_ATTR_CLR_FLAGS:
+		PRINT_FIELD_FLAGS(*attr, value, kfd_svm_flag, "KFD_IOCTL_SVM_FLAG_???");
+		break;
+	default:
+		PRINT_FIELD_X(*attr, value);
+	}
+	tprint_struct_end();
+}
+
+static void
+print_kfd_svm_args_attrs(struct tcb *tcp, const kernel_ulong_t arg, size_t nattr)
+{
+	const kernel_ulong_t attrs = arg + sizeof(struct kfd_ioctl_svm_args);
+	struct kfd_ioctl_svm_attribute attr;
+
+	tprints_field_name("attrs");
+	tprint_array_begin();
+	for (size_t i = 0; i < nattr; ++i) {
+		tfetch_obj(tcp, attrs + i * sizeof(struct kfd_ioctl_svm_attribute), &attr);
+		print_kfd_svm_attribute(&attr);
+	}
+	tprint_array_end();
+}
+
+#include "xlat/kfd_svm_op.h"
+
+static int
+print_amdkfd_ioc_svm(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_svm_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		if (args.op == KFD_IOCTL_SVM_OP_GET_ATTR) {
+			tprint_value_changed();
+			tprint_struct_begin();
+			print_kfd_svm_args_attrs(tcp, arg, args.nattr);
+			tprint_struct_end();
+		}
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_X(args, start_addr);
+	tprint_struct_next();
+	PRINT_FIELD_X(args, size);
+	tprint_struct_next();
+	PRINT_FIELD_XVAL(args, op, kfd_svm_op, "KFD_IOCTL_SVM_OP_???");
+	tprint_struct_next();
+	PRINT_FIELD_U(args, nattr);
+	if (args.op == KFD_IOCTL_SVM_OP_SET_ATTR) {
+		tprint_struct_next();
+		print_kfd_svm_args_attrs(tcp, arg, args.nattr);
+	}
+	tprint_struct_end();
+	return 0;
+}
+
+static int
 print_amdkfd_ioc_set_xnack_mode(struct tcb *const tcp,
 	const kernel_ulong_t arg)
 {
@@ -528,6 +902,77 @@ print_amdkfd_ioc_set_xnack_mode(struct tcb *const tcp,
 	return 0;
 }
 
+#include "xlat/kfd_criu_op.h"
+
+static int
+print_amdkfd_ioc_criu_op(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_criu_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	tprint_struct_begin();
+	PRINT_FIELD_PTR(args, devices);
+	tprint_struct_next();
+	PRINT_FIELD_PTR(args, bos);
+	tprint_struct_next();
+	PRINT_FIELD_PTR(args, priv_data);
+	tprint_struct_next();
+	PRINT_FIELD_X(args, priv_data_size);
+	tprint_struct_next();
+	PRINT_FIELD_U(args, num_devices);
+	tprint_struct_next();
+	PRINT_FIELD_U(args, num_bos);
+	tprint_struct_next();
+	PRINT_FIELD_U(args, num_objects);
+	tprint_struct_next();
+	PRINT_FIELD_U(args, pid);
+	tprint_struct_next();
+	if (entering(tcp))
+		PRINT_FIELD_XVAL(args, op, kfd_criu_op, "KFD_CRIU_OP_???");
+	tprint_struct_end();
+	return 0;
+}
+
+static int
+print_amdkfd_ioc_available_memory(struct tcb *const tcp,
+	const kernel_ulong_t arg)
+{
+	struct kfd_ioctl_get_available_memory_args args;
+
+	if (entering(tcp))
+		tprint_arg_next();
+	else if (syserror(tcp))
+		return RVAL_IOCTL_DECODED;
+	else
+		tprint_value_changed();
+
+	if (umove_or_printaddr(tcp, arg, &args))
+		return RVAL_IOCTL_DECODED;
+
+	if (exiting(tcp)) {
+		tprint_struct_begin();
+		PRINT_FIELD_X(args, available);
+		tprint_struct_end();
+		return RVAL_IOCTL_DECODED;
+	}
+
+	/* entering */
+	tprint_struct_begin();
+	PRINT_FIELD_X(args, gpu_id);
+	tprint_struct_end();
+	return 0;
+}
+
 int
 kfd_ioctl(struct tcb *const tcp, const unsigned int code,
 	const kernel_ulong_t arg)
@@ -543,16 +988,20 @@ kfd_ioctl(struct tcb *const tcp, const unsigned int code,
 		return print_amdkfd_ioc_set_memory_policy(tcp, arg);
 	case AMDKFD_IOC_GET_CLOCK_COUNTERS:
 		return print_amdkfd_ioc_get_clock_counters(tcp, arg);
-	/* TODO: AMDKFD_IOC_GET_PROCESS_APERTURES */
-	/* TODO: AMDKFD_IOC_UPDATE_QUEUE */
+	case AMDKFD_IOC_GET_PROCESS_APERTURES:
+		return print_amdkfd_ioc_get_process_apertures(tcp, arg);
+	case AMDKFD_IOC_UPDATE_QUEUE:
+		return print_amdkfd_ioc_update_queue(tcp, arg);
 	case AMDKFD_IOC_CREATE_EVENT:
 		return print_amdkfd_ioc_create_event(tcp, arg);
 	case AMDKFD_IOC_DESTROY_EVENT:
 		return print_amdkfd_ioc_destroy_event(tcp, arg);
 	case AMDKFD_IOC_SET_EVENT:
 		return print_amdkfd_ioc_set_event(tcp, arg);
-	/* TODO: AMDKFD_IOC_RESET_EVENT */
-	/* TODO: AMDKFD_IOC_WAIT_EVENTS */
+	case AMDKFD_IOC_RESET_EVENT:
+		return print_amdkfd_ioc_reset_event(tcp, arg);
+	case AMDKFD_IOC_WAIT_EVENTS:
+		return print_amdkfd_ioc_wait_events(tcp, arg);
 	/* TODO: DEPRECATED ioctl */
 	case AMDKFD_IOC_SET_SCRATCH_BACKING_VA:
 		return print_amdkfd_ioc_set_scratch_backing_va(tcp, arg);
@@ -572,10 +1021,26 @@ kfd_ioctl(struct tcb *const tcp, const unsigned int code,
 		return print_amdkfd_ioc_map_memory_to_gpu(tcp, arg);
 	case AMDKFD_IOC_UNMAP_MEMORY_FROM_GPU:
 		return print_amdkfd_ioc_unmap_memory_from_gpu(tcp, arg);
-	/* TODO: remaining ioctls */
+	case AMDKFD_IOC_SET_CU_MASK:
+		return print_amdkfd_ioc_set_cu_mask(tcp, arg);
+	case AMDKFD_IOC_GET_QUEUE_WAVE_STATE:
+		return print_amdkfd_ioc_get_queue_wave_state(tcp, arg);
+	case AMDKFD_IOC_GET_DMABUF_INFO:
+		return print_amdkfd_ioc_get_dmabuf_info(tcp, arg);
+	case AMDKFD_IOC_IMPORT_DMABUF:
+		return print_amdkfd_ioc_import_dmabuf(tcp, arg);
+	case AMDKFD_IOC_ALLOC_QUEUE_GWS:
+		return print_amdkfd_ioc_alloc_queue_gws(tcp, arg);
+	case AMDKFD_IOC_SMI_EVENTS:
+		return print_amdkfd_ioc_smi_events(tcp, arg);
+	case AMDKFD_IOC_SVM:
+		return print_amdkfd_ioc_svm(tcp, arg);
 	case AMDKFD_IOC_SET_XNACK_MODE:
 		return print_amdkfd_ioc_set_xnack_mode(tcp, arg);
-	/* TODO: remaining ioctls */
+	case AMDKFD_IOC_CRIU_OP:
+		return print_amdkfd_ioc_criu_op(tcp, arg);
+	case AMDKFD_IOC_AVAILABLE_MEMORY:
+		return print_amdkfd_ioc_available_memory(tcp, arg);
 	}
 	return RVAL_DECODED;
 }
